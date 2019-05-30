5625ca04fc5dc2b07582fb88e50db2f007ffde0d
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-05-30 22:14:54 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -8,10 +8,7 @@ def second_challenge
 
   #code your solution here!
   finalArray=[]
-  groceries.each do |key, val|
-    x=key.values 
-    finalArray=finalArray + x 
-  end
-  return finalArray.uniq
+  
+  
 
 end
